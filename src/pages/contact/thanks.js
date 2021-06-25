2c2f558bdfd1d9a373559b6bd4dddafa8202000c
Sam <hjiang129@gmail.com>
2021-06-24 21:02:10 -0400

    thank you button

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
 import Layout from '../../components/Layout'
 
 export default () => (
@@ -6,8 +8,11 @@ export default () => (
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1>Thank you for getting in touch!</h1>
+          <p>We will respond to your inquiry as soon as we can.</p>
+          <Link to="/" className="button is-primary">
+            Return Home
+          </Link>
         </div>
       </div>
     </section>
