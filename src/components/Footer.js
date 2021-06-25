f74744744c345beb808ce71e5cbe72cc20142916
Sam <hjiang129@gmail.com>
2021-06-24 20:09:53 -0400

    rename product to project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -142,7 +142,7 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/products">
+                      <Link className="navbar-item" to="/projects">
                         Projects
                       </Link>
                     </li>
